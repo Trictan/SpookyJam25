15365965f463386e9c5fadd1744393dfecadebb2
Tristan Harbander <tbd.mini@gmail.com>
2025-10-18 20:07:50 +0200

    abc

---
+++ tyroid_object.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var objs = {
+	"screw": {
+		"good": "model1",
+		"bad": "model2",
+		"maybe": "model3"
+	}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
